dafd3de9f2c7dac4e56031e3202ecf715dfd12c6
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-03-22 08:42:49 -0300

    correção de erro

--- src/views/AutorView.vue
+++ src/views/AutorView.vue
@@ -49,7 +49,7 @@ async function excluir(id) {
   <ul>
     <li v-for="autor in autores" :key="autor.id">
       <span @click="editar(autor)">
-        ({{ autor.id }}) - {{ autor.nome }} - {{ autor.site }} -
+        ({{ autor.id }}) - {{ autor.nome }} - {{ autor.email }} -
       </span>
       <button @click="excluir(autor.id)">X</button>
     </li>
